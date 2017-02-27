4570cb298daca41259130a103549cf828e2096cf
ad8 <chrisjhirsch@gmail.com>
2017-02-26 20:39:05 -0500

    Updating and fresh start on FastLED 3.1.3

---
+++ LEDsign.ino
@@ -0,0 +1,49 @@
+#include <bitswap.h>
+#include <chipsets.h>
+#include <color.h>
+#include <colorpalettes.h>
+#include <colorutils.h>
+#include <controller.h>
+#include <cpp_compat.h>
+#include <dmx.h>
+#include <FastLED.h>
+#include <fastled_config.h>
+#include <fastled_delay.h>
+#include <fastled_progmem.h>
+#include <fastpin.h>
+#include <fastspi.h>
+#include <fastspi_bitbang.h>
+#include <fastspi_dma.h>
+#include <fastspi_nop.h>
+#include <fastspi_ref.h>
+#include <fastspi_types.h>
+#include <hsv2rgb.h>
+#include <led_sysdefs.h>
+#include <lib8tion.h>
+#include <noise.h>
+#include <pixelset.h>
+#include <pixeltypes.h>
+#include <platforms.h>
+#include <power_mgt.h>
+
+
+const uint8_t kMatrixWidth  = 30;
+const uint8_t kMatrixHeight = 15;
+
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+#define DATA_PIN     7
+#define CLOCK_PIN     8
+#define BRIGHTNESS  32
+#define LED_TYPE    LPD8806
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+}
